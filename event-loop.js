e6b0e6e9455ad0cd2b811b2aa6111d60858a02a2
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-15 11:20:09 +0430

    if we use the Sync encryption so it will no longer run in the event loop and it will not offload to thread pool instead it blocks the code for each single encryption and then run the other codes

--- event-loop.js
+++ event-loop.js
@@ -1,6 +1,5 @@
 const fs = require("fs")
 const crypto = require("crypto")
-
 const start = Date.now();
 
 setTimeout(() => console.log('Timer 1 finished'), 0)
@@ -16,18 +15,15 @@ fs.readFile("test-file.txt", () => {
 
     process.nextTick(() => console.log('Process.nextTick'))
 
-    crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-        console.log(Date.now() - start, 'Password encrypted');
-    })
-    crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-        console.log(Date.now() - start, 'Password encrypted');
-    })
-    crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-        console.log(Date.now() - start, 'Password encrypted');
-    })
-    crypto.pbkdf2('password', 'salt', 100000, 1024, "sha512", () => {
-        console.log(Date.now() - start, 'Password encrypted');
-    })
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, "sha512")
+    console.log(Date.now() - start, 'Password encrypted');
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, "sha512")
+    console.log(Date.now() - start, 'Password encrypted');
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, "sha512")
+    console.log(Date.now() - start, 'Password encrypted');
+    crypto.pbkdf2Sync('password', 'salt', 100000, 1024, "sha512")
+    console.log(Date.now() - start, 'Password encrypted');
+
 })
 
 console.log("Hello from the top level code...");
