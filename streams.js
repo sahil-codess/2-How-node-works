1305344144264d633565881f588ac9dcb564feb8
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-15 17:17:37 +0430

    we can use streams instead of solution 1 and it works great it gets the data piece by peice and displays it to the user

--- streams.js
+++ streams.js
@@ -1,11 +1,26 @@
 const fs = require("fs");
 const server = require("http").createServer();
 
-server.on('Request', (req, res) => {
+server.on("request", (req, res) => {
     // solution 1
-    fs.readFile('test-file.txt', (err, data) => {
-        if (err) console.log(err);
-        res.end(data);
+    // fs.readFile('test-file.txt', (err, data) => {
+    //     if (err) console.log(err);
+    //     res.end(data);
+    // })
+
+    // solution 2: Streams
+    const readable = fs.createReadStream("test-file.txt");
+    readable.on("data", chunk => {
+        res.write(chunk);
+    })
+
+    readable.on("end", () => {
+        res.end();
+    })
+    readable.on("error", (err) => {
+        console.log(err);
+        res.statusCode = 500;
+        res.end("File not found!")
     })
 })
 
