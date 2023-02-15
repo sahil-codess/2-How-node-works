40d127c26d952c3a80342b478e4b460d7f26d3db
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-15 18:55:31 +0430

    exports is way easier than module.export

--- modules.js
+++ modules.js
@@ -1,8 +1,13 @@
 //console.log(arguments);
 // console.log(require("module").wrapper);
 
-
+// module.exports
 const C = require('./test-module-1');
 const calc1 = new C();
 
 console.log(calc1.add(2, 8));
+
+
+// exports
+const calc2 = require('./test-module-2')
+console.log(calc2.add(2, 8));
